3d62657d72ba4f7791737a2d030ab78a5d7f81d4
Opjasson <azmitheusos333@gmail.com>
2024-12-09 15:51:39 +0700

    modularisasi style.css

--- Contact-App/src/index.jsx
+++ Contact-App/src/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ContactApp from "./ContactApp";
 
+import './style.css'
+
 const element = <h1>Hello, world!</h1>;
 
 const root = createRoot(document.getElementById("root"));
