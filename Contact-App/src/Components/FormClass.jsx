d553f272f8d7dde656155524ee6915dddc8ae9b9
Opjasson <azmitheusos333@gmail.com>
2024-12-10 16:06:52 +0700

    create form using class component

--- Contact-App/src/Components/FormClass.jsx
+++ Contact-App/src/Components/FormClass.jsx
@@ -5,25 +5,76 @@ export default class FormClass extends Component {
         super(props);
 
         this.state = {
+            name: "",
             email: "",
+            gender: ""
         };
 
+        this.onNameChangeHandler = this.onNameChangeHandler.bind(this);
         this.onEmailChangeHandler = this.onEmailChangeHandler.bind(this);
+        this.onGenderChangeHandler = this.onGenderChangeHandler.bind(this);
+        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
     }
-    onEmailChangeHandler(event) {
+
+    onNameChangeHandler(event) {
         this.setState(() => {
             return {
+                name : event.target.value
+            }
+        })
+    }
+
+    onEmailChangeHandler(event) {
+         this.setState(() => {
+           return {
                 email: event.target.value,
             };
         });
+    }
+
+    onGenderChangeHandler(event) {
+        this.setState(() => {
+            return {
+                gender : event.target.value
+            }
+        })
+    }
+
+    onSubmitEventHandler(event) {
+        event.preventDefault();
+        
+        const message = `
+            Name : ${this.state.name}
+            Email : ${this.state.email}
+            Gender : ${this.state.gender}
+        `
+
+        return alert(message);
     }
 
     render() {
         return (
             <div>
                 <form action="">
-                    <label htmlFor="">Email</label>
-                    <input type="text" value={this.state.email} onChange={this.onEmailChangeHandler} />
+                    <div>
+                        <h1> Register Form</h1>
+                        <form>
+                            <label htmlFor="name">Name: </label>
+                            <input id="name" type="text" value={this.state.name} onChange={this.onNameChangeHandler} />
+                            <br />
+                            <label htmlFor="email">Email: </label>
+                            <input id="email" type="text" value={this.state.email} onChange={this.onEmailChangeHandler}/>
+                            <br />
+                            <label htmlFor="gender">Gender: </label>
+                            <select id="gender" value={this.state.gender} onChange={this.onGenderChangeHandler}>
+                                <option selected>Jenis kelamin</option>
+                                <option value="Man">Man</option>
+                                <option value="Woman">Woman</option>
+                            </select>
+                            <br />
+                            <button type="submit" onClick={this.onSubmitEventHandler}>submit</button>
+                        </form>
+                    </div>
                 </form>
             </div>
         );
