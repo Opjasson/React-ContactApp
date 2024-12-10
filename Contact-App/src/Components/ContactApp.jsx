b6f78ab67102b0d78ea08191c8114e03a2f5e504
Opjasson <azmitheusos333@gmail.com>
2024-12-10 14:18:31 +0700

    clear onDeleteHandler

--- Contact-App/src/Components/ContactApp.jsx
+++ Contact-App/src/Components/ContactApp.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import { getData } from "../Utils/js/data";
 import ContactList from "./ContactList";
 
-function ContactApp() {
-    const contacts = getData();
-    return (
-        <div className="contact-app">
-            <h1>Daftar Kontak</h1>
-            <ContactList contacts={contacts} />
-        </div>
-    );
+class ContactApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: getData(),
+        };
+
+        this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    }
+
+    onDeleteHandler(id) {
+        const contacts = this.state.contacts.filter(
+            (contact) => contact.id !== id
+        );
+        this.setState({ contacts });
+    }
+    render() {
+        return (
+            <div className="contact-app">
+                <h1>Daftar Kontak</h1>
+                <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+            </div>
+        );
+    }
 }
+
+// function ContactApp() {
+//     const contacts = getData();
+//     return (
+//         <div className="contact-app">
+//             <h1>Daftar Kontak</h1>
+//             <ContactList contacts={contacts} />
+//         </div>
+//     );
+// }
 
 export default ContactApp;
